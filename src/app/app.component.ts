82c6e8d42a500684cb17318f49417b4386d2f7bc
Dan Zou <danzou193@gmail.com>
2024-09-06 21:30:12 -0400

    Add customer name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,30 @@ import { CartComponent } from './cart/cart/cart.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormControl } from '@angular/forms';
 
 @Component({
+  imports: [
+    MenuComponent,
+    CartComponent,
+    MatSidenavModule,
+    MatButton,
+    MatIcon,
+    MatFormField,
+    MatInput,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   selector: 'app-root',
   standalone: true,
-  imports: [MenuComponent, CartComponent, MatSidenavModule, MatButton, MatIcon],
-  templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  templateUrl: './app.component.html',
 })
 @Injectable({ providedIn: 'root' })
 export class AppComponent {
@@ -20,6 +36,7 @@ export class AppComponent {
   constructor(private http: HttpClient) {}
 
   @ViewChild(MenuComponent) menuComponent!: MenuComponent;
+  customerName: FormControl = new FormControl("");
 
   submitOrder(): void {
     this.http
@@ -29,7 +46,7 @@ export class AppComponent {
       .subscribe((buffer) => console.log(buffer));
 
     let body = {
-      name: 'Dan',
+      name: this.customerName.value,
       items: this.menuComponent.menuItemComponents
         .map((item, _1, _2) =>
           Array(item.quantity).fill({
@@ -41,6 +58,9 @@ export class AppComponent {
 
     this.http
       .post('http://sidewalks-imac.local:5001/order', body)
-      .subscribe((_) => this.menuComponent.resetItems());
+      .subscribe((_) => {
+        this.menuComponent.resetItems();
+        this.customerName.setValue("");
+      });
   }
 }
